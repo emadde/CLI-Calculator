1b1c0f3e3056f4ec03647299551ef26f3162c915
ApoorvaShukla88 <Apoorvashukla88@gmail.com>
2020-08-11 00:58:54 -0400

    part2 done

--- src/part2/updatedOperations.java
+++ src/part2/updatedOperations.java
@@ -10,41 +10,58 @@ public class updatedOperations {
 				"m. Multiplication" + '\n' + "d. Division" + '\n' + "e. Exit");
 		System.out.println("Choose your operation: ");
 		
+		
+		// Prompt user for opertion
+		// store the operation in operation variable
+		// check which operation user wants to perform
+		// if operation is valid perform opertion
+		// else exit
+		
+		
 		Scanner input = new Scanner(System.in);
 		char operation = input.next().charAt(0);
-		System.out.println("First Number : ");	
-		double a = input.nextDouble();
 		
-		System.out.println("Second Number : ");
-		double b = input.nextDouble();
+		while(operation != 'e') {
 		
-		double result = 0;
-//		while(operation == 'e') {
-		switch(operation) {
-		case 'a':
-			result = a + b;
-			System.out.println("Result of " + a + " + " + b + " = " + result);
-			break;
-		case 's':
-			result = a - b;
-			System.out.println("Result of " + a + " - " + b + " = " + result);
-			break;
-		case 'm':
-			result = a * b;
-			System.out.println("Result of " + a + " * " + b + " = " + result);
-			break;
-		case 'd':
-			if(b == 0) System.out.println("Cannot divide by 0");
-			else {
-				result = a / b;
-			System.out.println("Result of " + a + " / " + b + " = " + result);
+			System.out.println("First Number : ");	
+			double a = input.nextDouble();
+		
+			System.out.println("Second Number : ");
+			double b = input.nextDouble();
+		
+			double result = 0;
+		
+		
+			if (operation == 'a') {
+				result = a + b;
+				System.out.println("Result of " + a + " + " + b + " = " + result);
 			}
-			break;
-		case 'e':
-			break;
+			else if (operation == 's') {
+				result = a - b;
+				System.out.println("Result of " + a + " - " + b + " = " + result);
 			}
+			else if (operation == 'm') {
+				result = a * b;
+				System.out.println("Result of " + a + " * " + b + " = " + result);
+			}
+			else if (operation == 'd') {
+				if	(b == 0) {
+					System.out.println("Cannot divide by 0");
+				} else {
+					result = a / b;
+					System.out.println("Result of " + a + " / " + b + " = " + Math.round(result * 100d) / 100d);
+				}
+			}
+			System.out.println("Choose your operation: ");
+			operation = input.next().charAt(0);
+		}
+		
 			System.out.println("Goodbye");
+			System.exit(0);
+			}
+		}
 			
-		}
-		}
+			
+		
+		
 	
